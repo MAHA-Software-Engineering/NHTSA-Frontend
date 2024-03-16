8228a448d1a162ae00f392fc15ac3df6fa1755aa
Matthew Quijano <mq022002@gmail.com>
2024-03-15 23:11:07 -0400

    refactor: Revert AWS Amplify integration
    - Moving forward with alternative way of integrating AWS Cognito into our project through NextAuth.js.

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,6 +1,5 @@
 import BaseLayout from "../../components/layouts/BaseLayout";
-import React, { useState } from "react";
-import { Auth } from "aws-amplify"
+import React from "react";
 
 function LoginPage() {
   return (
@@ -62,56 +61,5 @@ function LoginPage() {
     </BaseLayout>
   );
 }
-
-const User = {
-  email: string,
-  password: string
-};
-const LoginPage = () => {
-  const [user, setUser] = useState<User>({
-    email: "",
-    password: "",
-  });
-  const handleInputChange = (event) => {
-    setUser({ ...user, [event.target.name]: event.target.value });
-  };
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try{
-    // Send the user data to the server to login the user
-      await Auth.signIn(formik.values.email, formik.values.password)
-      console.log("User logged in:", user);
-    } catch(e){
-    concole.log(e)
-    }
-  };
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Email:
-        <input
-          type="email"
-          name="email"
-          value={user.email}
-          onChange={handleInputChange}
-          required
-        />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input
-          type="password"
-          name="password"
-          value={user.password}
-          onChange={handleInputChange}
-          required
-        />
-      </label>
-      <br />
-      <button type="submit">Login</button>
-    </form>
-  );
-};
 
 export default LoginPage;
--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,6 +1,5 @@
-import React, { useState} from "react";
+import React from "react";
 import BaseLayout from "../../components/layouts/BaseLayout";
-import { Auth } from "aws-amplify"
 
 function RegisterPage() {
   return (
@@ -9,63 +8,5 @@ function RegisterPage() {
     </BaseLayout>
   );
 }
-
-const User = {
-  email: string,
-  password: string
-};
-const RegisterPage = () => {
-  const [user, setUser] = useState<User>({
-    email: "",
-    password: "",
-  });
-  const handleInputChange = (event) => {
-    setUser({ ...user, [event.target.name]: event.target.value });
-  };
-  const handleSubmit = async(event) => {
-    event.preventDefault();
-    try {
-      const result = await Auth.signUp({
-        username: user.email,
-        password: user.password,
-        // if custom attribute is added
-        attributes: {
-          "custom:role": "user",
-        },
-      })
-      return result
-    } catch (error) {
-      console.error("Error registering user:", error)
-    }
-    console.log("User registered:", user);
-  };
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Email:
-        <input
-          type="email"
-          name="email"
-          value={user.email}
-          onChange={handleInputChange}
-          required
-        />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input
-          type="password"
-          name="password"
-          value={user.password}
-          onChange={handleInputChange}
-          required
-        />
-      </label>
-      <br />
-      <button type="submit">Register</button>
-    </form>
-  );
-};
 
 export default RegisterPage;

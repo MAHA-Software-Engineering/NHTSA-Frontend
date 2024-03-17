0c44fae89756bb97c0f4b6f4e6a893e5fa5be275
ABaerga2002 <a.baerga2002@gmail.com>
2024-03-17 14:11:23 -0400

    add: Added Cognito local variables to authenticate through the API

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth"
+import Providers from "next-auth/providers*"
+
+export default NextAuth(
+    {
+        providers: [
+            Providers.Cognito(
+                {
+                    clientID: process.env.COGNITO_CLIENT_ID,
+                    clientSecret: process.env.COGNITO_CLIENT_SECRET,
+                    domain: process.env.COGNITO_DOMAIN
+                }
+            )
+        ]
+    }
+)

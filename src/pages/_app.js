145afc60116568f8b6640d706cfaf72952fe917a
ABaerga2002 <a.baerga2002@gmail.com>
2024-03-17 14:27:47 -0400

    refactor: Moved app function to be a part of the export default

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,6 @@
 import '../styles/globals.css';
 import { Provider } from "next-auth/client/_utils";
 
-function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return (<Provider session = {pageProps.session}><Component {...pageProps}/></Provider>);
 }
-
-export default App;

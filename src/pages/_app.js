ab1275f4a635db6c9d1169e507afdc8b8aedee00
ABaerga2002 <a.baerga2002@gmail.com>
2024-03-17 14:10:59 -0400

    refactor: Re-imported next-auth/clients/_util to provide HomePage authentication

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,8 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { Provider } from "next-auth/client/_utils";
 
-export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
-  }
+function App({ Component, pageProps }) {
+  return (<Provider session = {pageProps.session}><Component {...pageProps}/></Provider>);
+}
+
+export default App;
